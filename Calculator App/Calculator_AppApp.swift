ffa6c2dc4d30855bd3e06596a4c2bf4f34790d1b
Aditya Wahyu Pratama <adityawahyu131@gmail.com>
2023-07-23 20:34:24 +0700

    Initial Commit

---
+++ Calculator App/Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_AppApp.swift
+//  Calculator App
+//
+//  Created by Adit on 23/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
